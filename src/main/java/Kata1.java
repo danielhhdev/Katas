9a27d283e47baafc93ce2217acadcae1debdb3dd
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-09-15 19:01:39 +0200

    commit

--- src/main/java/Kata1.java
+++ src/main/java/Kata1.java
@@ -1,3 +1,36 @@
+
+
+/*
+Debe escribir una función patternque devuelva el siguiente patrón
+(consulte Patrón y ejemplos) hasta nel número de filas.
+patrón (5):
+
+    1
+    22
+    333
+    4444
+    55555
+ */
 public class Kata1 {
-    
+    public static void main(String[] args) {
+
+    }
+
+    public static String pattern (int n){
+
+        int variable =0;
+        String cadena ="";
+        if (n<1){
+            return cadena;
+        }else{
+            for (int i = 1; i <=n ; i++) {
+
+                variable++;
+            }
+        }
+        return cadena;
+    }
+
+
+
 }
