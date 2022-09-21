978e28a9423c42ed4c8d640478146cb75ed65819
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-09-21 17:58:56 +0200

    kata3

--- src/main/java/Kata3.java
+++ src/main/java/Kata3.java
@@ -1,12 +1,20 @@
-import java.util.Arrays;
 
+/*
+
+Implemente la función que toma una matriz que contiene los nombres de las personas a las que les gusta un artículo.
+Debe devolver el texto de la pantalla como se muestra en los ejemplos:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+ */
 public class Kata3 {
 
     public static String whoLikesIt(String... names) {
 
         int contador= names.length-2;
-
-        //if(names.length==0) return "no one likes this";
 
         if(names.length==1) return names[0]+" likes this";
 
--- src/test/java/Kata3Test.java
+++ src/test/java/Kata3Test.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Kata3Test {
+    @Test
+    public void staticTests() {
+        assertEquals("no one likes this", Kata3.whoLikesIt());
+        assertEquals("Peter likes this", Kata3.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", Kata3.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", Kata3.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", Kata3.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+    }
+
 }
