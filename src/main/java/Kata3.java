0b7d4af1778764b527f84bf92c927cd2940ec512
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-09-21 17:55:53 +0200

    Test kata3

--- src/main/java/Kata3.java
+++ src/main/java/Kata3.java
@@ -1,11 +1,24 @@
+import java.util.Arrays;
+
 public class Kata3 {
 
     public static String whoLikesIt(String... names) {
 
+        int contador= names.length-2;
+
+        //if(names.length==0) return "no one likes this";
+
+        if(names.length==1) return names[0]+" likes this";
+
+        if(names.length==2) return names[0]+" and "+ names[1]+ " like this";
+
+        if(names.length==3) return names[0]+", "+names[1]+" and "+ names[2]+ " like this";
+
+        if(names.length>3) return names[0]+", "+ names[1]+ " and "+contador+" others like this";
+
+        return "no one likes this";
 
 
-
-        return "";
     }
 
 
---
+++ src/test/java/Kata3Test.java
@@ -0,0 +1,2 @@
+public class Kata3Test {
+}
