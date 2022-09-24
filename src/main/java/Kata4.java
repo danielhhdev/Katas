86a52de4c4133580a6e2393ddf0ea71385087558
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-09-24 17:34:07 +0200

    test kata4

--- src/main/java/Kata4.java
+++ src/main/java/Kata4.java
@@ -1,7 +1,28 @@
 
 /*
-
+Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales no son anagrama.
  */
 
 public class Kata4 {
+
+    public static Boolean isAnagrama (String word1, String word2){
+
+        StringBuilder newWord= new StringBuilder("");
+        word2.toLowerCase();
+        for (int i=word2.length();i>=0;i--){
+            newWord.append(word2.charAt(i));
+        }
+
+        if (word1.toLowerCase().equals(newWord)){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+
 }
---
+++ src/test/java/Kata4Test.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class Kata4Test {
+    @Test
+    public void testkata4(){
+
+    }
+
+}
