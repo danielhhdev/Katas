7764cdd099fcf4b09325f31bab68b0d1489e3e19
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-10-03 13:59:25 +0200

    Test Kata 7

---
+++ src/test/java/Kata7Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Kata7Test {
+
+    @Test
+    public void testkata6(){
+        assertEquals("odnum aloH",Kata7.invertidorCadena("Hola mundo"));
+        assertEquals("leinad",Kata7.invertidorCadena("daniel"));
+
+    }
+}
