60a48cb955558739315177e9cc595447c487e8b7
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-10-03 12:34:48 +0200

    Kata 6

---
+++ src/main/java/Kata6.java
@@ -0,0 +1,16 @@
+
+/*
+Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
+public class Kata6 {
+
+    public static boolean esPrimo(){
+        return false;
+    }
+
+
+
+
+}
