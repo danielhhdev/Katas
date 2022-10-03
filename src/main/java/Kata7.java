5b169c335bc372571d809051c39f0bc800f32374
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-10-03 13:57:04 +0200

    KAta 7

---
+++ src/main/java/Kata7.java
@@ -0,0 +1,20 @@
+/*
+Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ */
+
+public class Kata7 {
+
+
+    public static String invertidorCadena(String s){
+        String cadenaNueva="";
+        for (int i=s.length()-1;i>=0;i--){
+            cadenaNueva+=s.charAt(i);
+        }
+        return cadenaNueva;
+
+    }
+
+}
+
+
